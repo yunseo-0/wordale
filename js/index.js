46ecd5d4a405582459afe9953d39b5c1b2acc6cb
yunseo-0 <sy31004@naver.com>
2024-08-15 15:22:43 +0900

    [워들]
    워들 게임을 만들었습니다.

---
+++ js/index.js
@@ -0,0 +1,89 @@
+const 정답 = "APPLE";
+
+let index = 0;
+let attempts = 0;
+let timer;
+
+function appstart() {
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임 종료";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; position: absolute; top:40vh; left:40vw; background-color:white; width:200px height:100px;";
+    document.body.appendChild(div);
+  };
+  const nextLine = () => {
+    if (attempts === 6) return;
+    attempts += 1;
+    index = 0;
+  };
+  const gameover = () => {
+    window.removeEventListener("keydown", handlekeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+  const handleEnterKey = () => {
+    let 정답_갯수 = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-column[data-index='${attempts}${i}']`
+      );
+      const 입력_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+      if (입력_글자 === 정답_글자) {
+        정답_갯수 += 1;
+        block.style.background = "#6AAA64";
+      } else if (정답.includes(입력_글자)) block.style.background = "#C9B458";
+      else block.style.background = "#787C7E";
+      block.style.color = "white";
+    }
+    if (정답_갯수 === 5) gameover();
+    else nextLine();
+  };
+
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-column[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
+
+  const handlekeydown = (event) => {
+    const key = event.key.toUpperCase();
+    const keyCode = event.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-column[data-index='${attempts}${index}']`
+    );
+
+    if (event.key === "Backspace") handleEnterKey();
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index = index + 1;
+    }
+  };
+
+  const startTimer = () => {
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector(".time");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
+  window.addEventListener("keydown", handlekeydown);
+}
+
+appstart();
